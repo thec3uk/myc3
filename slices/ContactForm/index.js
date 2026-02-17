42812c0496e0fee4f49628733de827d920ea44b4
Andrew Miller <info@akmiller.co.uk>
2026-02-17 15:11:29 +0000

    Update index.js

--- slices/ContactForm/index.js
+++ slices/ContactForm/index.js
@@ -64,7 +64,7 @@ const ContactForm = ({ slice }) => {
     setFormState("submitting");
 
     try {
-      const res = await fetch("/__forms.html", {
+      const res = await fetch("/", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams({
